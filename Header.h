c7602edc1666d1ef3f80ec8bbd876781a2fcf679
OKRogova <81697553+OKRogova@users.noreply.github.com>
2021-04-05 21:37:59 +0500

    Initial commit

---
+++ Header.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+extern "C" __declspec(dllexport) int Group(int a);
+extern "C" __declspec(dllexport) int getName(char* A);
---
+++ pch.cpp
@@ -0,0 +1,21 @@
+﻿// pch.cpp: файл исходного кода, соответствующий предварительно скомпилированному заголовочному файлу
+
+#include "pch.h"
+#include <string>
+#pragma warning(disable : 4996)
+using namespace std;
+// При использовании предварительно скомпилированных заголовочных файлов необходим следующий файл исходного кода для выполнения сборки.
+extern "C" __declspec(dllexport) int Group(int a)
+{
+	a = 561220;
+	return a;
+}
+
+
+extern "C" __declspec(dllexport) int getName (char* A)
+{
+	const char* B = { "Rogova Olga Konstantinovna" };
+	int Razmer = sizeof(B);
+	strcpy(A, B);
+	return Razmer;
+}
